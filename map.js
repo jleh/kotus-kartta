3748046fdf506baadfce21a44fa45806e4651d18
Juuso Lehtinen <juuso.lehtinen@karttalehtinen.fi>
2017-09-10 13:02:48 +0300

    fixed build config & indent

--- map.js
+++ map.js
@@ -1,17 +1,19 @@
-var L = require('leaflet');
+require('proj4leaflet');
+var L = require('leaflet-mml-layers');
+
 var leafletMap;
 
 require('leaflet/dist/leaflet.css');
 
 L.Marker.prototype.options.icon = L.icon({
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 });
 
 var LAYERS = {
-    openstreetmap: L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    })
+  openstreetmap: L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  })
 };
 
 /*
@@ -22,25 +24,27 @@ Options
 */
 
 export function init(divId, options) {
-    leafletMap = L.map(divId)
-        .setView([options.latlng[0], options.latlng[1]], 10);
+  leafletMap = L.map(divId, {
+    crs: L.TileLayer.MML.get3067Proj()
+  })
+  .setView([options.latlng[0], options.latlng[1]], 10);
 
-    leafletMap.addLayer(LAYERS.openstreetmap);
-    leafletMap.addLayer(createKotusLayer(options));
-    L.marker([options.latlng[0], options.latlng[1]]).addTo(leafletMap);
+  leafletMap.addLayer(LAYERS.openstreetmap);
+  leafletMap.addLayer(createKotusLayer(options));
+  L.marker([options.latlng[0], options.latlng[1]]).addTo(leafletMap);
 }
 
 function createKotusLayer(options) {
-    var wmsOptions = {
-        layers: 'kotus:new_tif',
-        format: 'image/png',
-        transparent: true,
-        elevation: options.mapId ? options.mapId : '1/100000'
-    };
+  var wmsOptions = {
+    layers: 'kotus:new_tif',
+    format: 'image/png',
+    transparent: true,
+    elevation: options.mapId ? options.mapId : '1/100000'
+  };
 
-    if (options.years) {
-        wmsOptions['time'] = options.years[0] + '-1-1/' + options.years[1] + '-1-1'
-    }
+  if (options.years) {
+    wmsOptions['time'] = options.years[0] + '-1-1/' + options.years[1] + '-1-1'
+  }
 
-    return L.tileLayer.wms('https://avoin-test.csc.fi/geoserver/kotus/wms', wmsOptions);
+  return L.tileLayer.wms('https://avoin-test.csc.fi/geoserver/kotus/wms', wmsOptions);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -9,29 +9,24 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        },
-        {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader'
-            ]
-       }
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: './build/'
+            }
+          }
+        ]
+      }
     ]
-  },
-  resolve: {
-    // Leaflet image Alias resolutions
-    alias: {
-      './images/layers.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/layers.png'),
-      './images/layers-2x.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/layers-2x.png'),
-      './images/marker-icon.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/marker-icon.png'),
-      './images/marker-icon-2x.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/marker-icon-2x.png'),
-      './images/marker-shadow.png$': path.resolve(__dirname, './node_modules/leaflet/dist/images/marker-shadow.png')
-    }
   }
 }
